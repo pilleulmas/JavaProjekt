8fdd89110e86a61379f201c69fe2943d9dd5cc07
pilleulmas <pilleulmas@gmail.com>
2016-12-29 18:35:40 +0200

    FX pusimine

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,9 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import javax.swing.event.TreeModelEvent;
@@ -19,11 +19,60 @@ public class Main extends Application {
 
        // Stage stage = new Stage();
        // StackPane stack = new StackPane();
+        Label pealkiri = new Label("Vali maksimaalne ja tehte liik!");
+
+        HBox valik = new HBox();
+        valik.setAlignment(Pos.BOTTOM_CENTER);
+        valik.setSpacing(20);
+
+        int max =10;
+        Label slText = new Label();
+        slText.setText("Vali maksimaalne\nkordaja/summa");
+        slText.setAlignment(Pos.BOTTOM_CENTER);
+        Label slmax = new Label();
+        Slider slider = new Slider();
+        slider.setMin(0);
+        slider.setMax(100);
+        slider.setValue(10);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+        slider.setMajorTickUnit(10);
+        slider.setOrientation(Orientation.VERTICAL);
+        slider.valueProperty().addListener((observable, vanaVaartus, uusVaartus) -> {
+
+            slmax.setText("Max on " + uusVaartus.intValue());
+            //max = slider.valueProperty().intValue();
+        });
 
 
-        VBox vbox = new VBox();
-        vbox.setSpacing(20);
-        Scene esimene = new Scene(vbox, 400, 300);
+        Button valiV22rtus = new Button();
+        valiV22rtus.setText("Kinnita valik!");
+        valiV22rtus.setOnAction((event) -> {
+            //max.setValue(slider).intValue();
+                System.out.println("Vajutasid nupule!");
+
+        });
+        VBox vasak = new VBox();
+        vasak.setAlignment(Pos.TOP_CENTER);
+        vasak.getChildren().addAll(slText, slider, slmax, valiV22rtus);
+
+        GridPane kesk = new GridPane();
+        kesk.setAlignment(Pos.TOP_CENTER);
+        kesk.add(pealkiri, 1, 0,4,1);
+        for (int i=1; i<6; i++) {
+            Label yl = new Label("Ülesanne " +Integer.toString(i));
+            kesk.add(yl, 0, i);
+            TextField vastus = new TextField("Vastus "+ Integer.toString(i));
+            kesk.add(vastus,1,i);
+            Label oige = new Label("Õige vastus " + Integer.toString(i));
+            kesk.add (oige,2,i);
+        }
+
+        BorderPane aken = new BorderPane();
+        aken.setTop(valik);
+        aken.setCenter(kesk);
+        aken.setLeft(vasak);
+        Scene esimene = new Scene(aken, 800, 600);
         primaryStage.show();
 
         ToggleGroup g = new ToggleGroup();
@@ -49,27 +98,30 @@ public class Main extends Application {
             System.out.println(g.getSelectedToggle().getUserData().toString());
             switch (g.getSelectedToggle().getUserData().toString()) {
                 case "Liitmine":
-                    System.out.println("Liitmine, hurraa!");;
+                    pealkiri.setText("LIITMINE");
+                    //liitmine();
                     break;
                 case "Lahutamine":
-                    System.out.println("Lahutamine, hurraa!");;
+                    pealkiri.setText("LAHUTAMINE");
                     break;
                 case "Korrutamine":
-                    System.out.println("Korrutamine, hurraa!");;
+                    pealkiri.setText("KORRUTAMINE");
                     break;
                 case "Jagamine":
-                    System.out.println("Jagamine, hurraa!");;
+                    pealkiri.setText("JAGAMINE");
                     break;
                 case "V6rdlemine":
-                    System.out.println("Võrdlemine, hurraa!");;
+                    pealkiri.setText("VÕRDLEMINE");
                     break;
                 case "J2rjestamine":
-                    System.out.println("Järjestamine, hurraa!");;
+                    pealkiri.setText("JÄRJESTAMINE");
+                    //j2rjestamine();
                     break;
             }
         });
 
-        vbox.getChildren().addAll(e1, e2, e3, e4, e5, e6);
+        valik.getChildren().addAll(e1, e2, e3, e4, e5, e6);
+
 
 
         primaryStage.setScene(esimene);
